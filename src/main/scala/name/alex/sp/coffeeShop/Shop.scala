5906bf34702dd7657cc85cbd1d172801740f8c8d
Alex <alex@plato>
2014-05-25 08:12:58 +0100

    Shop gets a staff member to make the coffee.

--- src/main/scala/name/alex/sp/coffeeShop/Shop.scala
+++ src/main/scala/name/alex/sp/coffeeShop/Shop.scala
@@ -1,14 +1,15 @@
 package name.alex.sp.coffeeShop
 
-import java.util.concurrent.{TimeUnit, Executors}
+import java.util.concurrent.{ScheduledExecutorService, TimeUnit, Executors}
 import scala.concurrent._
 
 import name.alex.sp.FunctionalUtils
 import FunctionalUtils._
 
 class Shop {
-
   private[this] val executor = Executors.newScheduledThreadPool(/* corePoolSize */ 0)
+
+  implicit def getExecutionContext: ExecutionContext = JavaConversions.asExecutionContext(executor)
 
   def order(request: OrderRequest): OrderProgressCustomer = {
     val progress = new OrderProgressStaff(request)
@@ -17,8 +18,23 @@ class Shop {
     progress.forCustomer
   }
 
+  private def beginGetStaffMember(): Future[Staff] = {
+    val p = Promise[Staff]()
+    executor.schedule(toRunnable (() => p.success(new Staff())), 200, TimeUnit.MILLISECONDS)
+    p.future
+  }
+
   private[this] def beginFulfilment(progress: OrderProgressStaff) = {
-    executor.schedule(toRunnable (() => progress.allFinishedItems.success(Seq(CoffeeItem()))), 200, TimeUnit.MILLISECONDS)
+    val f: Future[Staff] = beginGetStaffMember()
+    f.onSuccess {
+      case staff => staff.beginFulfilment(progress)
+    }
+  }
+
+  private class Staff() {
+    def beginFulfilment(progress: OrderProgressStaff) {
+      executor.schedule(toRunnable (() => progress.allFinishedItems.success(Seq(CoffeeItem()))), 200, TimeUnit.MILLISECONDS)
+    }
   }
 }
 
