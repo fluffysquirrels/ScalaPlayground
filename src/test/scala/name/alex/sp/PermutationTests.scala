85e04793e65a71e940a3d4cc92d824a0dd9e45db
Alex <alex@plato>
2014-05-27 22:39:31 +0100

    Tidied up permutation tests.

--- src/test/scala/name/alex/sp/PermutationTests.scala
+++ src/test/scala/name/alex/sp/PermutationTests.scala
@@ -4,8 +4,57 @@ import org.scalatest.{FreeSpec, Matchers}
 
 
 class PermutationTests extends FreeSpec with Matchers with name.alex.sp.util.Assertions {
+  {
+    val it = Vector()
+    it.toString() - {
 
-  def getEm(elts: Vector[Int]): Seq[Seq[Int]] = {
+      "its permutations should" - {
+        val perms = getPermutations(it)
+
+        collectionsEqual(perms,
+          Vector()
+        )
+      }
+    }
+  }
+
+  {
+    val it = Vector(1)
+    it.toString() - {
+
+      "its permutations should" - {
+        val perms = getPermutations(it)
+
+        collectionsEqual(perms,
+          Vector(
+            Vector(1)
+          )
+        )
+      }
+    }
+  }
+
+  {
+    val it = Vector(1, 2, 3)
+    it.toString() - {
+      "its permutations should" - {
+        val perms = getPermutations(it)
+
+        collectionsEqual(perms,
+          Vector(
+            Vector(1, 2, 3),
+            Vector(1, 3, 2),
+            Vector(2, 1, 3),
+            Vector(2, 3, 1),
+            Vector(3, 1, 2),
+            Vector(3, 2, 1)
+          )
+        )
+      }
+    }
+  }
+
+  def getPermutations(elts: Vector[Int]): Seq[Seq[Int]] = {
     if(elts.isEmpty) {
       Vector()
     }
@@ -16,26 +65,7 @@ class PermutationTests extends FreeSpec with Matchers with name.alex.sp.util.Ass
       headIx <- 0 until elts.length;
       head = elts(headIx);
       rest = elts.patch(headIx, Vector(), 1);
-      subPerm <- getEm(rest)
+      subPerm <- getPermutations(rest)
     ) yield Vector(head) ++ subPerm
-  }
-
-  "1,2,3" - {
-    val it = Vector(1,2,3)
-
-    "its permutations should" - {
-      val perms = getEm(it)
-
-      collectionsEqual(perms,
-        Vector(
-          Vector(1, 2, 3),
-          Vector(1, 3, 2),
-          Vector(2, 1, 3),
-          Vector(2, 3, 1),
-          Vector(3, 1, 2),
-          Vector(3, 2, 1)
-        )
-      )
-    }
   }
 }
